43a721b1ec728965732bcfacb441e5fe8acfa668
Andrew DGalga <ex.spake@gmail.com>
2023-08-28 14:59:27 -0500

    added js image positioning & modified styling for more consistent highlight color.

--- scripting.js
+++ scripting.js
@@ -1,3 +1,4 @@
+//Thanks Kevin Powell on YouTube
 const track = document.getElementsByClassName('atxgd_carousel-images')[0];
 const images = Array.from(track.children);
 const rightBtn = document.getElementsByClassName('atxgd_carousel-btnright')[0];
@@ -5,3 +6,8 @@ const leftBtn = document.getElementsByClassName('atxgd_carousel-btnleft')[0];
 const navBar = document.getElementsByClassName('atxgd_carousel-nav-btns')[0];
 const posIndicators = Array.from(navBar.children);
 const imageWidth = images[0].getBoundingClientRect().width;
+
+const setImagePositon = (image, index) => {
+    image.style.left = `${imageWidth * index}px`;
+};
+images.forEach(setImagePositon);
