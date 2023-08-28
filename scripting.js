e2f330fb1debce7c7ed1d201a69f114ef2788bf3
Andrew DGalga <ex.spake@gmail.com>
2023-08-28 14:44:01 -0500

    js variables. scripting is not built to support multiple carousels on a page, but leaving HTML+CSS structure as-is in case I want to figure that part out after this project. Easier to redo JS than all 3 together.

--- scripting.js
+++ scripting.js
@@ -1,2 +1,7 @@
-//const track = document.
-
+const track = document.getElementsByClassName('atxgd_carousel-images')[0];
+const images = Array.from(track.children);
+const rightBtn = document.getElementsByClassName('atxgd_carousel-btnright')[0];
+const leftBtn = document.getElementsByClassName('atxgd_carousel-btnleft')[0];
+const navBar = document.getElementsByClassName('atxgd_carousel-nav-btns')[0];
+const posIndicators = Array.from(navBar.children);
+const imageWidth = images[0].getBoundingClientRect().width;
