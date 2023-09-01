d6c7c1907416c8d2ea07b50f197a95874cc7b8e2
Andrew DGalga <ex.spake@gmail.com>
2023-09-01 07:03:36 -0500

    visibility in CSS and visibility swap on mobile button.

--- scripting.js
+++ scripting.js
@@ -1,4 +1,11 @@
 /*Mobile nav button______________________________________________________________________*/
+const mobileBtn = document.getElementById('atxgd_mobile-burger');
+const navMenu = document.getElementsByClassName('atxgd_nav-menu')[0];
+const computedMenuStyle = window.getComputedStyle(navMenu);
+
+mobileBtn.addEventListener('click', ()=>{
+    (computedMenuStyle.getPropertyValue('visibility') === 'hidden') ? navMenu.style.visibility = 'visible' : navMenu.style.visibility = 'hidden';
+});
 
 /*Carousel_______________________________________________________________________________*/
 const carouselTrack = document.getElementsByClassName('atxgd_carousel-images')[0];
